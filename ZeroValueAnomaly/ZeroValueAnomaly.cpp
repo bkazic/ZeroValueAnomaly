f5e4d50346468630dc64dfab3c1a527d44c5f2d3
Blaz Kazic <blaz.kazic@qlector.com>
2019-02-11 15:48:27 +0100

    All the necessary building blocks

--- ZeroValueAnomaly/ZeroValueAnomaly.cpp
+++ ZeroValueAnomaly/ZeroValueAnomaly.cpp
@@ -1,9 +1,40 @@
 #include "base.h"
-
+#include "misc.cpp"
 
 int main()
 {
-	TStr Bla = "4343";
-	printf("%s\n", Bla.CStr());
+	// Init zero values matrix
+	TFltVV TestMat = TFltVV(3, 2);
+    //TFltVVV TestMat3D = TFltVVV(2, 2, 2);
+
+	// Tests
+    Misc::MatInfo(TestMat);
+    Misc::PrintMat(TestMat);
+    Misc::RndMat(TestMat);
+    Misc::PrintMat(TestMat);
+    Misc::LineReader("test.txt");
+    Misc::Timestamps();
+
+    // Test reading input data
+    PJsonVal DataJson = Misc::JsonFileReader("json.txt");
+    TFltVV DataMat = Misc::JsonArr2TFltVV(DataJson);
+    Misc::PrintMat(DataMat);
+    TFltVVV Model = Misc::Model(DataMat);
+    Misc::Print3DMat(Model);
+
+    // Test reading input data
+    //DataJson = Misc::JsonFileReader("json2.txt");
+    //DataMat = Misc::JsonArr2TFltVV(DataJson);
+    //Misc::PrintMat(DataMat);
+    //Model = Misc::Model(DataMat);
+    //Misc::Print3DMat(Model);
+
+    //// Test reading input data
+    DataJson = Misc::JsonFileReader("dummy.csv");
+    DataMat = Misc::JsonArr2TFltVV(DataJson);
+    //Misc::PrintMat(DataMat);
+    Model = Misc::Model(DataMat, false);
+    Misc::Print3DMat(Model);
+
 	return 0;
 }
